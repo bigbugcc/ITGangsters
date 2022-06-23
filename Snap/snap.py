24a76c3b0c72139194fb6a0dbb03e4352637f5c2
风吹我已散 <fcwys@qq.com>
2022-06-23 19:12:14 +0800

    完善对同地域多实例支持

--- Snap/snap.py
+++ Snap/snap.py
@@ -9,6 +9,7 @@ import os
 import sys
 import datetime
 import time
+import platform
 from tencentcloud.common import credential
 from tencentcloud.common.profile.client_profile import ClientProfile
 from tencentcloud.common.profile.http_profile import HttpProfile
@@ -20,13 +21,18 @@ sid=''
 skey=''
 region=''
 client=''
+instotal=0
+snaptotal=0
+pathtag='/'     # 用于Linux平台路径分割，Windows不需要
+
 # 当前程序路径
 baseDir=os.path.dirname(sys.argv[0])
 # 当天时间
-nowDay=time.strftime("%Y-%m-%d",time.localtime(time.time()))
+nowDay=time.strftime("%Y%m%d%H%M",time.localtime(time.time()))
 
 # 获取快照信息
 def getSnaps():
+    global snaptotal
     print('### 获取快照信息...')
     try:
         req = models.DescribeSnapshotsRequest()
@@ -41,24 +47,27 @@ def getSnaps():
         # print(resp)
         snapnum=0
         snaps=[]
-        # snaptotal=resp['TotalCount']    # 快照总数
+        snaptotal=resp['TotalCount']    # 快照总数
+        print('>>> 快照数量:',snaptotal)   
         for s in resp['SnapshotSet']:
             snapnum+=1
             snaps.append(s['SnapshotId'])
             # print('快照序号:',snapnum)
             # print('快照 ID:',s['SnapshotId'])
+            # print('磁盘 ID:',s['DiskId'])
             # print('快照名称:',s['SnapshotName'])
             # print('快照状态:',s['SnapshotState'])
             # print('快照进度:',str(s['Percent'])+'%')
             # print('创建时间:',utc2local(s['CreatedTime']))
             # print('')
         print('>>> 快照列表:',snaps) 
-        return [snaps[-1]]      # 删除最旧的一个
+        return snaps
     except TencentCloudSDKException as err:
         print(err)
 
 # 获取轻量云实例ID
 def getInstances():
+    global instotal
     print('### 获取轻量云实例ID...')
     try:
         req = models.DescribeInstancesRequest()
@@ -71,13 +80,16 @@ def getInstances():
         # print(resp.to_json_string())
         resp = json.loads(resp.to_json_string())
         # print(resp)
-        insnum=0
+        insnum=0        
         inslist=[]
+        instotal=resp['TotalCount']    # 实例总数
+        print('>>> 实例数量:',instotal)
         for s in resp['InstanceSet']:
             insnum+=1
             inslist.append(s['InstanceId'])
             # print('实例序号:',insnum)
             # print('实例 ID:',s['InstanceId'])
+            # print('磁盘ID:',s['SystemDisk']['DiskId'])
             # print('实例名称:',s['InstanceName'])
             # print('实例区域:',s['Zone'])            
             # print('公网地址:',s['PublicAddresses'])
@@ -93,11 +105,11 @@ def getInstances():
 # 创建快照（实例 ID）
 def createSnap(inslist):
     print('### 创建快照...')
-    snapname="快照_" + nowDay   # 快照名称，可自定义
     try:
         req = models.CreateInstanceSnapshotRequest()
         # 遍历实例ID列表
         for i in inslist:
+            snapname=f'实例{i}_{nowDay}'   # 快照名称，可自定义
             params = {
                 "InstanceId": i,
                 "SnapshotName": snapname  
@@ -107,9 +119,7 @@ def createSnap(inslist):
             resp = json.loads(resp.to_json_string())
             # print(resp.to_json_string())
             if(resp['SnapshotId']!=''):
-                print('>>> 快照创建成功!')
-                print('>>> 快照 ID:', str(resp['SnapshotId']))
-                print('>>> 快照名称:', snapname)
+                print(f'>>> 快照创建成功,[ID] {resp["SnapshotId"]},[名称] {snapname},[关联实例ID] {resp["SnapshotId"]}')
     except TencentCloudSDKException as err:
         print(err)
 
@@ -144,9 +154,9 @@ def utc2local(utctime):
 def getConf():
     global sid,skey,region,client
     print('### 加载配置文件...')
-    if(os.path.exists(f'{baseDir}/snapconf.json')):
+    if(os.path.exists(f'{baseDir}{pathtag}snapconf.json')):
         # 配置文件存在
-        with open(f'{baseDir}/snapconf.json','r',encoding='utf8') as f:            
+        with open(f'{baseDir}{pathtag}snapconf.json','r',encoding='utf8') as f:            
             conf=json.loads(f.read().rstrip())
             f.close()
             sid=conf['SecretId']
@@ -166,17 +176,29 @@ def getConf():
             print(err)
     else:
         # 配置文件不存在
-        with open(f'{baseDir}/snapconf.json','w',encoding='utf8') as f:
+        with open(f'{baseDir}{pathtag}snapconf.json','w',encoding='utf8') as f:
             f.write('{"SecretId":"","SecretKey":"","Region":"ap-hongkong"}')
             f.close()            
             print('>>> 配置文件不存在,已重新生成!')
 
 # 主程序开始
 if __name__=='__main__':
-    print('========== 腾讯轻量云快照创建助手 ==========')
+    print('============== 腾讯轻量云快照助手 ==============')
+    # Windows平台去除路径/
+    if platform.system().lower() == "windows":
+        pathtag=''
     # 获取配置
     getConf()
-    # 删除最旧快照
-    delSanp(getSnaps())
+    # 获取实例
+    inslist=getInstances()
+    # 获取快照
+    snaplist=getSnaps()
+    # 判断快照数量
+    if(snaptotal>0 and snaptotal==1):
+        # 仅一个快照
+        delSanp(snaplist[-1])
+    elif(snaptotal>0 and snaptotal>instotal):
+        # 快照数大于实例数
+        delSanp(snaplist[instotal:])
     # 创建新快照
-    createSnap(getInstances())
+    createSnap(inslist)
